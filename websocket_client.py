12a40b252b33e51b169a80eaacdaa0aeef0169c8
Kevin Matthews <kmatthews@cyberdata.net>
2018-10-02 15:06:33 -0700

    Add notes on a websocket client in python

---
+++ websocket_client.py
@@ -0,0 +1,6 @@
+# eventlet.websocket does not seem to contain a method for creating a client websocket connection
+# Neither does the stock websocket module.
+# Can try using the websocket-client module (https://pypi.org/project/websocket-client/)
+# Can also try using SocketIO, but their docs seem to be for websocket servers.
+
+print('Not implemented. Load websocket_client.html in a browser instead.')
