39719136a9f722453ad48b3a32a039456e8ded40
Kevin Matthews <kmatthews@cyberdata.net>
2018-10-02 14:28:52 -0700

    Add WSGI application to websocket server
    No websockets yet.

--- websocket_server.py
+++ websocket_server.py
@@ -3,9 +3,19 @@ import eventlet
 from eventlet import wsgi
 from eventlet import websocket
 
+SERVER_HOSTNAME = '127.0.0.1'
+SERVER_PORT = 8090
 
-# wsgi: https://wsgi.readthedocs.io/en/latest/
-# http://eventlet.net/doc/modules/wsgi.html
+def application(environ, start_response):
+    response_body = ''.encode('utf-8')
+    status = '200 OK'
+    response_headers = [
+        ('Content-Type', 'text/plain'),
+        ('Content-Length', str(len(response_body))),
+    ]
+    start_response(status, response_headers)
+    return [response_body]
 
-# Maybe try this first:
-# http://eventlet.net/doc/examples.html#socket-connect
+if __name__ == '__main__':
+    listener = eventlet.listen((SERVER_HOSTNAME, SERVER_PORT))
+    wsgi.server(listener, application)
